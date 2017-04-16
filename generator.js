124ecf3c70dc43fdfd3649aacbb1f0f0404485d2
Anson Adams <ansonadams27@gmail.com>
2017-04-16 14:56:28 -0700

    Verified files hooked up properly (success!)

--- generator.js
+++ generator.js
@@ -1,1 +1,8 @@
 const ASTClasses = require('./ast.js');
+
+Object.assign(ASTClasses.Program.prototype, {
+    gen() {
+        //this.statements.forEach(statement => statement.gen());
+        return "This is placeholder output for the generator";
+    },
+});
